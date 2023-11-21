e98b88629e52f28bb9d68939f896fb26d4b53e0a
지수진 <zi_soo_zin@naver.com>
2023-11-21 20:31:37 +0900

    Initial commit

---
+++ src/components/ArticleEditForm.svelte
@@ -0,0 +1,18 @@
+<!-- slog-addForm start -->
+<div class="slog-add-content-box" >
+    <div class="content-box-header ">
+    <div class="flex" >
+        <p>지금 여러분의 생각을 적어주세요.</p>
+    </div>
+    </div>
+    <div class="content-box-main">
+    <textarea id="message" rows="5" class="slog-content-textarea " placeholder="내용을 입력해 주세요."></textarea>
+    </div>
+    <div class="content-box-bottom">
+    <div class="button-box">
+        <button class="button-base">입력</button>
+        <button class="button-base">취소</button>
+    </div>
+    </div>
+</div>
+<!-- slog-addForm end -->
---
+++ src/components/Comment.svelte
@@ -0,0 +1,18 @@
+<!-- comment start-->
+<li>
+    <div class="comment-top ">
+      <div class="comment-top-left  ">
+        <p class="p-user" >freeseamew</p>
+        <p class="p-date-comment" >2022-11-11</p>
+      </div>
+      <div class="comment-top-right ">
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-5 w-5" ><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>
+        </button>
+      </div>                
+    </div>
+    <div class="comment-bottom ">
+      <p class="whitespace-pre-line">안녕하세요. 첫번째 코멘트 입니다. 여러분의 생각을 적어주세요.</p>
+    </div>
+</li>
+<!-- comment end-->
