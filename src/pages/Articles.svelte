ebc312a926090686b10a43a065f9fe4b301b8916
지수진 <zi_soo_zin@naver.com>
2023-11-21 21:00:55 +0900

    [추가] page 컴포넌트 템플릿 추가

---
+++ src/pages/Articles.svelte
@@ -0,0 +1,11 @@
+<script>
+    import ArticleHeader from "../components/ArticleHeader.svelte";
+    import ArticleList from "../components/ArticleList.svelte";
+    import ArticleAddForm from "../components/ArticleAddForm.svelte";
+    import AuthLogin from "../components/AuthLogin.svelte";
+</script>
+
+<ArticleHeader/>
+<main class="auth-box">
+    <AuthLogin/>
+</main>
