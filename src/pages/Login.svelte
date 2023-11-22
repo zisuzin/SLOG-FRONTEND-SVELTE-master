8821b3479bb85ea4aa6b07d112efdc3f499ba31f
지수진 <zi_soo_zin@naver.com>
2023-11-22 16:35:51 +0900

    [변경] npm init 초기화

--- src/pages/Login.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import AuthHeader from "../components/AuthHeader.svelte";
-    import AuthLogin from "../components/AuthLogin.svelte";
-</script>
-
-<AuthHeader/>
-<main class="auth-box">
-    <AuthLogin/>
-</main>
--- src/pages/Register.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import AuthHeader from "../components/AuthHeader.svelte";
-    import AuthRegister from "../components/AuthRegister.svelte";
-</script>
-
-<AuthHeader/>
-<main class="auth-box">
-    <AuthRegister/>
-</main>
